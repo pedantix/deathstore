1d4189c32e7833fea0ce5f813cb4b663d4dc12c1
Shaun Hubbard <shaunhubbard2013@icloud.com>
2016-07-24 19:56:26 -0400

    links added (#13)
    * links added
    
    * satisfying the hound
    
    * took care of hound vios
    
    * fixed vio

--- app/controllers/directives_controller.rb
+++ app/controllers/directives_controller.rb
@@ -1,6 +1,7 @@
 class DirectivesController < ApplicationController
   helper_method :directive
-  before_action :authenticate_user!, except: :show
+  before_action :authenticate_user!, except: [:show, :qr_code]
+  include ApplicationHelper
 
   def show
   end
@@ -33,6 +34,13 @@ class DirectivesController < ApplicationController
     directive.destroy!
     flash[:success] = t(".success")
     redirect_to root_path
+  end
+
+  def qr_code
+    svg_file = Tempfile.new(["qr_code", ".svg"])
+    svg_file.write qr_helper(user)
+    send_file svg_file, type: "image/svg+xml",
+                        x_sendfile: true
   end
 
   private
---
+++ spec/controllers/directives_controller_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe DirectivesController do
+  let(:user) { create :user }
+  let(:qr_svg_xml) do
+    url = user_directives_url(user)
+    qrcode = RQRCode::QRCode.new(url)
+
+    qrcode.as_svg(offset: 0, color: "000", 
+                    shape_rendering: "crispEdges", 
+                    module_size: 9)
+  end
+
+  context "GET #qr_code" do
+    it "should respond with svg xml" do
+      get :qr_code, user_id: user.id
+
+      expect(subject).to respond_with :ok
+      expect(response.body).to eq qr_svg_xml
+    end
+  end
+end
